d8b606e174523be0a0775e84f29af6b1f538ec28
Ivan Gagis <igagis@gmail.com>
2024-12-24 20:30:24 +0200

    set opengl profile to core

--- src/main.cpp
+++ src/main.cpp
@@ -300,8 +300,9 @@ int main( int argc, char* args[] ) {
 	});
 	
 	// use OpenGL 2.0
-	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 2 ); 
-	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 0 );
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2); 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 	
 	SDL_Window* window = SDL_CreateWindow(
 			"SDL Tutorial",
